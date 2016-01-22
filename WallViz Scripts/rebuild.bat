a1521cc9df9a36be9576088915cce74d793b1aba
Martin Grunbaum <grunbaum@di.ku.dk>
2016-01-22 14:37:19 +0100

    Improved WallViz script
    - There is now an xmlBase folder for the shared xml files.
    - Each xmlX folder now contains only the changed files, app_settings and
      multiplexer_settings.xml

--- WallViz Scripts/rebuild.bat
+++ WallViz Scripts/rebuild.bat
@@ -5,9 +5,14 @@ xcopy "C:\Users\wallvizpc\workspace\ccv15\apps\addonsExamples\VS2010\bin" "C:\Us
 xcopy "C:\Users\wallvizpc\workspace\ccv15\apps\addonsExamples\VS2010\bin" "C:\Users\wallvizpc\Desktop\ccvRun\2\" /S /Y
 xcopy "C:\Users\wallvizpc\workspace\ccv15\apps\addonsExamples\VS2010\bin" "C:\Users\wallvizpc\Desktop\ccvRun\3\" /S /Y
 
-xcopy "C:\Users\wallvizpc\workspace\ccv15\WallViz Scripts\xml1" "C:\Users\wallvizpc\Desktop\ccvRun\1\data\xml\" /S /Y
-xcopy "C:\Users\wallvizpc\workspace\ccv15\WallViz Scripts\xml2" "C:\Users\wallvizpc\Desktop\ccvRun\2\data\xml\" /S /Y
-xcopy "C:\Users\wallvizpc\workspace\ccv15\WallViz Scripts\xml3" "C:\Users\wallvizpc\Desktop\ccvRun\3\data\xml\" /S /Y
+xcopy "C:\Users\wallvizpc\workspace\ccv15\WallViz Scripts\xmlBase" "C:\Users\wallvizpc\Desktop\ccvRun\1\data\xml\" /S /Y
+xcopy "C:\Users\wallvizpc\workspace\ccv15\WallViz Scripts\xml1\*.xml" "C:\Users\wallvizpc\Desktop\ccvRun\1\data\xml\" /S /Y
+
+xcopy "C:\Users\wallvizpc\workspace\ccv15\WallViz Scripts\xmlBase" "C:\Users\wallvizpc\Desktop\ccvRun\2\data\xml\" /S /Y
+xcopy "C:\Users\wallvizpc\workspace\ccv15\WallViz Scripts\xml2\*.xml" "C:\Users\wallvizpc\Desktop\ccvRun\2\data\xml\" /S /Y
+
+xcopy "C:\Users\wallvizpc\workspace\ccv15\WallViz Scripts\xmlBase" "C:\Users\wallvizpc\Desktop\ccvRun\3\data\xml\" /S /Y
+xcopy "C:\Users\wallvizpc\workspace\ccv15\WallViz Scripts\xml3\*.xml" "C:\Users\wallvizpc\Desktop\ccvRun\3\data\xml\" /S /Y
 
 @echo cd C:\Users\wallvizpc\Desktop\ccvRun\1\ >> "C:\Users\wallvizpc\Desktop\ccvRun\run.bat"
 @echo start C:\Users\wallvizpc\Desktop\ccvRun\1\CCV.exe >> "C:\Users\wallvizpc\Desktop\ccvRun\run.bat"
